d704699035302a0243a1e4953ddfd0aeb01b23aa
SimProch <mavian@email.cz>
2020-11-22 21:26:48 +0100

    Updated gitignore, moved last example

--- result/outputInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface outputInterface {
-    id: number;
-    description_name?: string;
-    when_joined?: Date;
-    bank_status?: number;
-    is_insured?: boolean;
-}
--- result/outputModel.cs
+++
@@ -1,7 +0,0 @@
-public class outputModel {
-    public int id { get; set; }
-    public string? description_name { get; set; }
-    public DateTime? when_joined { get; set; }
-    public double? bank_status { get; set; }
-    public bool? is_insured { get; set; }
-}
